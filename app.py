318a033dc7bd8529f8768c2c0f9412f9dc1aee3a
nikhilarya <nikhilarya31@gmail.com>
2025-01-03 19:42:47 +0530

    Added Jobs section

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+
 
 app = Flask(__name__)
 
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi, India',
+        'salary': 'Rs 15,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontent Engineer',
+        'location': 'San Fracisco, USA'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'Remote',
+        'salary': 'Rs 14,00,000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "Hekko world"
+    return render_template('home.html', jobs=JOBS)
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
